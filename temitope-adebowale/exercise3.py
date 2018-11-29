5891ed3c7685a2adccdcba021cc11f2efa00144c
Temitope-Adebowale <45217638+Temitope-Adebowale@users.noreply.github.com>
2018-11-29 10:47:50 +0100

    temitope-adebowale test

---
+++ temitope-adebowale/exercise3.py
@@ -0,0 +1,14 @@
+# write a function that takes your name
+# and returns the first and the last characters of your name
+# e.g. Chukwudi becomes Ci
+
+def myName(name):
+    return name[0] + name[-7]
+
+
+print(myName("Temitope"))
+
+
+
+
+
